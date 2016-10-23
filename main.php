3a9229824610986029593b8f7c13c20a251641f2
IsidorosK <ikoutsoumbos@hotmail.com>
2016-10-23 21:46:07 +0300

    Update and rename .gitignore to main.php

---
+++ main.php
@@ -0,0 +1,300 @@
+
+
+
+<html>
+<Title>Musicstore Sign-In</Title>
+<head>
+<link href='https://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet' type='text/css'>
+<meta charset="utf-8">
+
+
+<div class="main">
+
+</div>
+
+
+
+
+
+<style> 
+
+
+
+.cb-slideshow<!--,.cb-slideshow:after-->{
+	position:fixed;
+	width:100%;
+	height:100%;
+	top:0px;
+	left:0px;
+	z-index:0;
+}
+
+.cb-slideshow:after{
+content:'';
+background-image:url(http://jewelmagazineonline.com/wp-content/uploads/2015/11/adele.jpg)repeat top left;
+background-position:center-top;
+background-size:cover;
+transition:background-image 1s linear;
+}
+
+.cb-slideshow li span { 
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    color: transparent;
+    background-size: cover;
+    background-position: 50% 50%;
+    background-repeat: none;
+    opacity: 0;
+    z-index: 0;
+    animation: imageAnimation 36s linear infinite 0s; 
+}
+
+.cb-slideshow li span { 
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    color: transparent;
+    background-size: cover;
+    background-position: 50% 50%;
+    background-repeat: none;
+    opacity: 0;
+    z-index: 0;
+    animation: imageAnimation 36s linear infinite 0s; 
+}
+
+.cb-slideshow li div { 
+    z-index: 0;
+    position: absolute;
+    bottom: 10px;
+    left: 0px;
+    width: 100%;
+    text-align:centre;
+    opacity: 0;
+    color: #fff;
+    animation: titleAnimation 36s linear infinite 0s; 
+}
+.cb-slideshow li div h3 {
+	
+    font-family: 'BebasNeueRegular', 'Arial Narrow', Arial, sans-serif;
+    font-size: 195;
+	position:relative;
+	bottom:0;
+	width:100%;
+	text-align:center;
+	vertical-align:bottom;
+    padding: 0;
+    line-height: 200px; 
+}
+
+.cb-slideshow li:nth-child(1) span { 
+    background-image: url(http://jewelmagazineonline.com/wp-content/uploads/2015/11/adele.jpg) 
+}
+
+.cb-slideshow li:nth-child(2) span { 
+    background-image: url(http://www.fortitudemagazine.co.uk/wp-content/uploads/2015/10/David-Bowie.jpg);
+    animation-delay: 6s; 
+	}
+	
+.cb-slideshow li:nth-child(3) span { 
+    background-image: url(http://www.thefilmgrapevine.com/wp-content/uploads/2014/11/hans-zimmer.png);
+    animation-delay: 12s;
+}	
+
+.cb-slideshow li:nth-child(4) span { 
+    background-image: url(https://www.polaroidcube.com/wp-content/uploads/2015/07/Justice_in_concert.jpg);
+    animation-delay: 18s; 
+	}
+	
+.cb-slideshow li:nth-child(5) span { http://s5.favim.com/orig/53/beatles-black-and-white-boys-meninos-Favim.com-495303.jpg);
+    background-image: url( http://s5.favim.com/orig/53/beatles-black-and-white-boys-meninos-Favim.com-495303.jpg);
+    animation-delay: 24s; 
+}
+
+.cb-slideshow li:nth-child(6) span { 
+    background-image: url(https://wallpaperscraft.com/image/metallica_youth_band_members_haircut_3671_2560x1024.jpg);
+    animation-delay: 30s; 
+}
+
+.cb-slideshow li:nth-child(2) div { 
+    animation-delay: 6s; 
+}
+.cb-slideshow li:nth-child(3) div { 
+    animation-delay: 12s; 
+}
+.cb-slideshow li:nth-child(4) div { 
+    animation-delay: 18s; 
+}
+.cb-slideshow li:nth-child(5) div { 
+    animation-delay: 24s; 
+}
+.cb-slideshow li:nth-child(6) div { 
+    animation-delay: 30s; 
+}
+
+@keyframes imageAnimation { 
+    0% { opacity: 0; animation-timing-function: ease-in; }
+    8% { opacity: 1; animation-timing-function: ease-out; }
+    17% { opacity: 1 }
+    25% { opacity: 0 }
+    100% { opacity: 0 }
+}
+
+@keyframes titleAnimation { 
+    0% { opacity: 0 }
+    8% { opacity: 1 }
+    17% { opacity: 1 }
+    19% { opacity: 0 }
+    100% { opacity: 0 }
+}
+
+
+.body{
+  margin: 0;
+  background: #000; 
+  
+}
+
+h1{
+	margin-top:-90px;
+	display:block;
+	font-size:50;
+	font-family: 'Audiowide', cursive;
+ 
+	color:#7F00FF;
+	
+	text-align:center;
+	text-shadow:2px 2px dove gray;
+	}
+	
+h2{
+	margin-top:0px;
+	margin-left:-1200px;
+	font-size:39;
+	font-family:'Audiowide';
+	color:#7F00FF;
+	
+}
+
+input[type=text]{
+	display:table-cell;
+	font-family:'Audiowide';
+	padding:5px 20px;
+	border:2 groove;
+	border-color:black;
+	border-radius:5px;
+	background-color:#FAFAD2;
+	color:black;
+	
+	
+	
+}
+
+input[type=password]{
+	font-family:'Audiowide';
+	display:table-cell;
+	padding:5px 20px;
+	border:2 groove;
+	border-color:black;
+	background-color:#FAFAD2;
+	border-radius:5px;
+	
+}
+
+input[type=submit] {
+    padding:10px 25px; 
+	color:#7F00FF;
+	font-family:'Audiowide', cursive;
+	font-size:20;
+    background:black; 
+    border:2 none;
+    cursor:pointer;
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+	}
+	
+button[type=button] {
+	padding:10px 25px; 
+	color:#7F00FF;
+	font-family:'Audiowide', cursive;
+	font-size:20;
+    background:black; 
+    border:2 none;
+    cursor:pointer;
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+}
+b
+	{
+		font-size:47;
+		font-family:'Audiowide';
+		font-weight:bold;
+		border:5px dove gray;
+	}
+
+a{
+	position:relative;
+	z-index:999;
+
+}
+</style>
+</head>
+<body>
+
+<ul class="cb-slideshow">
+	<li>
+		<span>Image 01</span>
+		<div>
+			<h3><div class="cb-slideshow li div h3">A·D·E·L·E</h3></div>
+		</div>
+
+	</li>
+	<li>
+		<span>Image 02</span>
+		<div>
+			<h3><div class="cb-slideshow li div h3">B·O·W·I·E</h3></div>
+		</div>
+	</li>
+	<li>
+		<span>Image 03</span>
+		<div>
+			<h3><div class="cb-slideshow li div h3">H·A·N·S&nbsp; Z·I·M·M·E·R</h3></div>
+		</div>
+	</li>
+	<li>
+		<span>Image 04</span>
+		<div>
+			<h3><div class="cb-slideshow li div h3">R·O·L·L·I·N·G &nbsp; S·T·O·N·E·S</h3></div>
+		</div>
+	</li>
+	<li>
+		<span>Image 05</span>
+		<div>
+			<h3><div class="cb-slideshow li div h3">T·H·E &nbsp;  B·E·A·T·L·E·S</h3></div>
+		</div>
+	</li>
+	<li>
+		<span>Image 06</span>
+		<div>
+			<h3><div class="cb-slideshow li div h3">M·E·T·A·L·L·I·C·A</h3></div>
+		</div>
+	</li>
+	
+<a><div align=center><form action="signin.php" method="post">
+<h1>WELCOME TO CATHERINE & ISIDOR MUSIC E-SHOP!<br></h1>
+<br>
+<h2><b>Sign In</b><br>
+  &nbsp;&nbsp;&nbsp;  Username:<input type="text" name="username" value="" size=20><br>
+  &nbsp;&nbsp;&nbsp;  Password:<input type="password" name="password" value="" size=20><br>
+<input type="submit" value="Sign-In">
+<button type="button" onclick="location.href='register.php'">Register</button><br> </div></a></h2>
+</body>
+</html>
+
+
+	
+
